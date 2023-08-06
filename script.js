b0d88140265bcff273f0e7ad7963f00782cfac67
V1ZROY <vizoronah@googlemail.com>
2023-08-06 13:22:35 +0100

    Page structure and styling

---
+++ script.js
@@ -0,0 +1,13 @@
+let wordArray = ["tell", "very", "even", "back", "any", "good", "woman", "through", "life", "child", "work", "down", "may", "after", "should", "call", "world", "over", "school", "still", "try", "last"
+]
+
+let randomListButton = document.getElementById('new-list')
+
+randomListButton.addEventListener('click', test)
+
+function test(title, length, content) {
+    prompt("New list test");
+}
+
+
+  
