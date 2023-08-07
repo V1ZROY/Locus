1ddaf0f7eeba0f0b4548a7f76d358f70577e486e
V1ZROY <vizoronah@googlemail.com>
2023-08-07 08:06:26 +0100

    working on new list generation

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 let wordArray = ["tell", "very", "even", "back", "any", "good", "woman", "through", "life", "child", "work", "down", "may", "after", "should", "call", "world", "over", "school", "still", "try", "last"
 ]
 
-let randomListButton = document.getElementById('new-list')
+let newListButton = document.getElementById('new-list')
+let newListModal = document.getElementById('new-list-modal')
+let newListCloseButton = document.getElementById('new-list-close-button')
+let modalOverlay = document.getElementById('modal-overlay')
+let listsContainer = document.getElementById('lists-container')
+let playRandomListButton = document.getElementById('play-random-list')
 
-randomListButton.addEventListener('click', test)
+newListButton.addEventListener('click', showNewListForm)
+modalOverlay.addEventListener('click', hideNewListForm)
+newListCloseButton.addEventListener('click', hideNewListForm);
 
-function test(title, length, content) {
-    prompt("New list test");
+playRandomListButton.addEventListener('click', saveNewList)
+
+function showNewListForm() {
+    newListModal.classList.remove('modal-closed');
+    modalOverlay.classList.remove('modal-closed');
 }
 
+function hideNewListForm() {
+    newListModal.classList.add('modal-closed');
+    modalOverlay.classList.add('modal-closed');
+}
+
+
+function saveNewList() {
+    let newItemContainer = document.createElement("div")
+    listsContainer.appendChild(document.createElement("div"))
+    newItemContainer.classList.add('red')
+}
 
   
