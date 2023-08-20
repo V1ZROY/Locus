cfc47e35b3fa1bfdda33ccd2d4adee192bba3c9c
V1ZROY <vizoronah@googlemail.com>
2023-08-20 21:03:08 +0100

    Working list generation to page

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let newListCloseButton = document.getElementById('new-list-close-button')
 let modalOverlay = document.getElementById('modal-overlay')
 let listsContainer = document.getElementById('lists-container')
 let playRandomListButton = document.getElementById('play-random-list')
+let newListForm = document.getElementById('new-list-form')
 
 newListButton.addEventListener('click', showNewListForm)
 modalOverlay.addEventListener('click', hideNewListForm)
 newListCloseButton.addEventListener('click', hideNewListForm);
 
-playRandomListButton.addEventListener('click', saveNewList)
 
 function showNewListForm() {
     newListModal.classList.remove('modal-closed');
@@ -24,11 +24,68 @@ function hideNewListForm() {
     modalOverlay.classList.add('modal-closed');
 }
 
-
-function saveNewList() {
-    let newItemContainer = document.createElement("div")
-    listsContainer.appendChild(document.createElement("div"))
-    newItemContainer.classList.add('red')
+// Attempting to make a contructor function to store each list as an object
+class List {
+    listName = "";
+    identifier = "" + Date().toLocaleString();
+    length = 0;
+    contents = []
+    populateList(count){
+        let newListItem
+        while (count >= 1) {
+            newListItem = document.createElement("li")
+            newListContents.appendChild(newListItem)
+            newListItem.textContent = wordArray[Math.floor(Math.random()*wordArray.length)]
+            count --
+        };
+    };
 }
 
-  
+
+let newListNameTest = new List();
+console.log(newListNameTest)
+
+function saveNewList(newListForm) {
+    /*New list content*/
+    let newItemContainer = document.createElement("div")
+    let newListHeader = document.createElement("div")
+    let newListContents = document.createElement("ul")
+
+    /*New list header content*/
+    let newListName = document.createElement('h3')
+    let newListPlayButton = document.createElement('button')
+
+    /*Adding textcontent to new nodes*/
+    newListName.textContent = document.querySelector('input[name="new-list-name"]').value
+    document.querySelector('input[name="new-list-name"]').value = "";
+    newListPlayButton.textContent = 'Play'
+
+    /*adding classes to new nodes*/
+    newItemContainer.classList.add('item-container')
+    newListHeader.classList.add('list-header')
+    newListContents.classList.add('list-contents')
+    newListPlayButton.classList.add('play-list-button')
+
+    /*Adding new nodes to DOM*/
+    listsContainer.appendChild(newItemContainer).appendChild(newListHeader)
+    newItemContainer.appendChild(newListContents)
+    newListHeader.appendChild(newListName)
+    newListHeader.appendChild(newListPlayButton)
+
+    /*Call function to populate list*/
+    populateList(document.querySelector('input[name="new-list-length"]').value, newListContents)
+
+    /*Close new List modal after list is made*/
+    hideNewListForm()
+}
+
+function populateList(count, newListContents) {
+    let newListItem
+    while (count >= 1) {
+        newListItem = document.createElement("li")
+        newListContents.appendChild(newListItem)
+        newListItem.textContent = wordArray[Math.floor(Math.random()*wordArray.length)]
+        count --
+    }
+    document.querySelector('input[name="new-list-length"]').value = "";
+}
